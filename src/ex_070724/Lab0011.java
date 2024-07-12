a49d0671cc3c0a7be792fdf86f4067866c6122ad
Deepalihub <deepali51092@gmail.com>
2024-07-12 12:23:41 +0530

    new FILES ADDED

---
+++ src/ex_070724/Lab0011.java
@@ -0,0 +1,11 @@
+package ex_070724;
+
+public class Lab0011 {
+    public static void main(String[] Deepali) {
+        System.out.println("Helloo world");
+        System.out.print("Hello World");
+
+
+        System.out.println("hello");
+    }
+}
